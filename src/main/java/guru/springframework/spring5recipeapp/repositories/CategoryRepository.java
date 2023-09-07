9d1762a1a74ac66f3f68ce6d7bf91df1c27da2ba
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2023-09-07 15:51:03 -0600

    Spring Data JPA Query Methods

--- src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,9 +3,13 @@ package guru.springframework.spring5recipeapp.repositories;
 import guru.springframework.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by gd on 9/6/2023
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 
 }
--- src/main/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -1,11 +1,16 @@
 package guru.springframework.spring5recipeapp.repositories;
 
+import guru.springframework.spring5recipeapp.domain.Category;
 import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 /**
  * Created by gd on 9/6/2023
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 
 }
